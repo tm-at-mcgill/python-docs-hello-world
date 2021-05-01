3e05464624837f4daef2f683ed60bdf0bf6c6786
Thierry Mayrand <thierry.mayrand@mcgill.ca>
2021-05-01 01:47:34 -0400

    removed db

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+"""
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
@@ -68,3 +82,4 @@ def update(id):
 
 if __name__ == '__main__':
     app.run(debug=True)
+"""
